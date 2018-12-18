cc2d9aa9857a2261207337d569be5019d1f6ddbc
Freddy Pan <freddy45remix@gmail.com>
2018-12-18 10:16:30 +0800

    go module init

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/freddy45pan/go-demo-core
--- product/product.go
+++ product/product.go
@@ -9,11 +9,13 @@ type Product struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
 	Price int    `json:"price"`
+	Desc  string `json:"desc"`
 }
 
 // Read
 func (product *Product) Read() (err error) {
 	product.Title = "prod_" + product.ID
 	product.Price = rand.Intn(100)
+	product.Desc = "This is product '" + product.ID + "'"
 	return
 }
